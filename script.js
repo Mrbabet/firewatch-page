7f9462ef2f20c8c61f58d8dd1f0d8a19c4e67970
Mrbabet <wieliczkokamil@gmail.com>
2022-12-31 17:02:58 +0100

    Fix: fixed problem with paralax. Needed to add possition absolute on the header. Thanks to it white box with height of the header doesnt appear anymore.

--- script.js
+++ script.js
@@ -7,13 +7,14 @@ let paralax5 = document.getElementById("paralax5");
 let paralax6 = document.getElementById("paralax6");
 let paralax7 = document.getElementById("paralax7");
 let paralax8 = document.getElementById("paralax8");
+let paralax = document.getElementById("paralax");
 
 window.addEventListener("scroll", function () {
   let value = window.scrollY;
 
   paralax0.style.top = value * -0.02 + "px";
   paralax1.style.top = value * -0.05 + "px";
-  paralax2.style.top = value * 0.11 + "px";
+  paralax2.style.top = value * -0.11 + "px";
   paralax3.style.top = value * -0.16 + "px";
   paralax4.style.top = value * -0.26 + "px";
   paralax5.style.top = value * -0.36 + "px";
